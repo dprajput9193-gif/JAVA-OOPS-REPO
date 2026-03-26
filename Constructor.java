d29186d88002ffa71b73597666c10f6ac1d88147
Deepanshu <dprajput9193@gmail.com>
2026-03-26 21:34:01 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,14 @@
+class a{
+    a(){
+        System.out.println("Hello This is a Constructor : ");
+    }
+    public void show(){
+        System.out.println("Hello Deepanshu ");
+    }
+}
+public class Constructor {
+    public static void main(String[] args) {
+        a x = new a();
+        x.show();
+    }
+}
